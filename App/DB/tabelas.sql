ae66448d99ab7468aaaae14d108c9552824a5317
edufrasson <edudomine2016@gmail.com>
2022-10-31 08:29:06 -0300

    mudança no banco de dados

--- App/DB/tabelas.sql
+++ App/DB/tabelas.sql
@@ -15,3 +15,13 @@ create table noticia(
     primary key(id),
     foreign key(id_categoria) references categoria_noticia(id)     
 );
+
+create table usuario(
+    id int auto_increment not null,
+    email varchar(100) not null,
+    nome varchar(100) not null,
+    senha varchar(100) not null, 
+    primary key(id)
+);
+
+INSERT INTO usuario(nome, email, senha) VALUES("Administrador", "adm@email", sha1(123));
